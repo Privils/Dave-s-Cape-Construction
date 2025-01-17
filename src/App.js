0bf34ab38cdf6ac0bfe3f97938b16099996d06f4
Priviledge <privilledgeblack8@gmail.com>
2025-01-18 00:28:18 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
-
-
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
   <>
   <Header />
   <Home />
+  <Footer />
   </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+
+
+import React from 'react' 
+import { FaEnvelope, FaPhone, FaInstagram, FaFacebook } from 'react-icons/fa'
+
+
 
 const Footer = () => {
   return (
-    <div>
+   <>
+    <section className="footer">
+      <p>DM-Projects <span>the dream maker for all your home needs</span></p>
       
-    </div>
+      <div className="footer-content">
+        <div className="contact">
+          <h1>Contact Us</h1>
+          <p><FaEnvelope /> contact@compan.co.za</p>
+          <p><FaPhone /> +27 889 5624 527</p>
+          <p><FaInstagram /> michaels_562@twed</p>
+          <p><FaFacebook /> Michael-john@facenool</p>
+        </div>
+
+        <div className="links">
+          <h1>Quick Links</h1>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-services">
+          <h1>Our Services</h1>
+          <ul>
+            <li>New Construction</li>
+            <li>Renovations & Remodeling</li>
+            <li>Additions & Extensions</li>
+            <li>Property Maintenance</li>
+            <li>Landscaping & Hardscaping</li>
+            <li>Foundation & Concrete Services</li>
+            <li>Demolition Services</li>
+            <li>Roofing Services</li>
+            <li>Plumbing & Electrical Services</li>
+            <li>Drywall & Painting</li>
+            <li>Flooring Installation & Repairs</li>
+            <li>Windows & Doors</li>
+            <li>Handyman Services</li>
+            <li>Project Management & Consultation</li>
+            <li>Cleaning Services</li>
+          </ul>
+        </div>
+
+      </div>
+    </section>
+   </>
   )
 }
 
