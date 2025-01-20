44096518933739a59ff59a63fa9f9abc58e61356
Priviledge <privilledgeblack8@gmail.com>
2025-01-20 19:14:05 +0200

    fix error

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   <Header/>
   <Routes>
 <Route index element={<Home/>}/>
+<Route path="/Home" element={<Home/>}/>
 <Route path='/Portfolio' element={<Portfolio/>}/>
 <Route path='/services/Additions' element={<Additions/>}/>
   </Routes>
