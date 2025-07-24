a5144845ef6d6b5546a1258aa6736e4b984ae373
Priviledge <privilledgeblack8@gmail.com>
2025-07-24 15:38:21 +0200

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,10 @@ import React, { useEffect, useState } from "react";
 import { FaPhone, FaClock, FaEnvelope, FaBars, FaWindowClose } from "react-icons/fa";
 import logo from "./images/logo.jpg";
 import { Link, useLocation } from "react-router-dom";
-import image1 from './images/background1.jpg';
-import image2 from './images/background2.jpg';
-import image3 from './images/background3.jpeg';
-import image4 from './images/background4.jpg';
-import image5 from './images/header-image-1.jpg';
 
 const Header = () => {
   const [isSticky, setIsSticky] = useState(false);
   const [menuActive, setMenuActive] = useState(false);
-
-  const images = [
-    { src: image1, alt: "construction" },
-    { src: image2, alt: "construction" },
-    { src: image3, alt: "construction" },
-    { src: image4, alt: "construction" },
-    { src: image5, alt: "construction" }
-  ];
-
-  const [randomImage, setRandom] = useState(images[0].src);
-  const [fade, setFade] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,65 +20,38 @@ const Header = () => {
     setMenuActive((prev) => !prev);
   };
 
-  const getRandomImages = () => {
-    const index = Math.floor(Math.random() * images.length);
-    const randomImage = images[index];
 
-    setFade(true);
-    setTimeout(() => {
-      setRandom(randomImage.src);
-      setFade(false);
-    }, 500);
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(getRandomImages, 7000);
-    return () => clearInterval(intervalId);
-  }, []);
 
   const location = useLocation();
   const currentPath = location.pathname;
 
   return (
    <>
-    <header style={{ backgroundImage: `url(${randomImage})` }}>
-      <div className="header-div">
-        <div className="header-icon-divs">
-          <FaPhone className="fa-phone" />
-          <p>
-            Get in touch <br />
-            <span>+27 74 929 4332</span>
-          </p>
-        </div>
-        <div className="header-icon-divs">
-          <FaEnvelope className="fa-envelope" />
-          <p>
-            Email us at <br />
-            <span>mushuredavid@gmail.com</span>
-          </p>
-        </div>
-        <div className="header-icon-divs">
-          <FaClock className="fa-clock" />
-          <p>
-            Working Hours <br />
-            <span>08:00 - 16:00</span>
-          </p>
-        </div>
-      </div>
-
-      <nav id="nav" className={isSticky ? "sticky" : ""}>
-        <div className={isSticky ? "nav-div sticky" : "nav-div"}>
-          <Link to="/">
+    <header>
+      <nav className={isSticky ? "sticky" : ""}>
+         <div className="logo-container">
+           <Link to="/">
             <img src={logo} alt="Logo" className="logo" />
           </Link>
-          <FaBars className="checkbtn" onClick={toggleMenu} />
+          <h1>dave's cape <br/>construction</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos ullam enim culpa tempore deserunt odit id quos aperiam velit pariatur nihil, cumque eum sint quasi labore unde eius accusamus vero.
+          Labore, molestiae quia. Assumenda minima reiciendis totam, iste a consectetur, doloremque culpa numquam eligendi velit facilis, repudiandae adipisci sapiente quo quod. Facere ea maiores omnis quisquam nisi animi expedita dolorem!
+          Illum, similique excepturi! Magni, exercitationem iusto sit maxime quibusdam dolorem quod suscipit maiores voluptatum. quisquam at a eum animi. Cumque, nobis veniam.</p>
+             <button className="header-btn">get a free qoute</button>
+         </div>
+          {/* <FaBars className="checkbtn" onClick={toggleMenu} /> */}
           <ul className={menuActive ? "active" : ""}>
-            <FaWindowClose className="close" onClick={toggleMenu} />
+            {/* <FaWindowClose className="close" onClick={toggleMenu} /> */}
 
             {currentPath === "/" ? (
               // Show all links except "Home" when on the home page
               <>
-                <li className="lists">
+              <li className="dropdown">
+                <Link to="#">
+                services
+                </Link>
+                <ul className="dropdown-menu">
+                   <li className="lists">
                   <Link to="/portfolio" onClick={toggleMenu}>Portfolio</Link>
                 </li>
                 <li className="lists">
@@ -106,6 +63,8 @@ const Header = () => {
                 <li className="lists">
                   <a href="#contact" onClick={toggleMenu}>Contact</a>
                 </li>
+                </ul>
+              </li>
               </>
             ) : (
               // Show only "Home" link when on any other page
@@ -113,8 +72,8 @@ const Header = () => {
                 <Link to="/" onClick={toggleMenu}>Home</Link>
               </li>
             )}
+            <button className="header-btn">get a free qoute</button>
           </ul>
-        </div>
       </nav>
     </header>
    </>
